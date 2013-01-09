889ecd63be450003078f09dc4b8a2a2d1088739e
olenhad <momeriqbal2@gmail.com>
2013-01-09 15:34:28 +0800

    added old team line up

---
+++ models/oldteams.js
@@ -0,0 +1,85 @@
+var teams = function () {
+    return [
+            {
+                "name": "BrainWave",
+                "app" : "Team Buddy",
+                "stack": "LAMP, GMaps API, Android",
+                "description": "A scheduling app for university students with project modules. Location- based meeting scheduler. Ensure that team members don’t mess around during meetings, and attend meetings etc. Planning on incorporate Latitude API, and to send SMS notifications. Can be extended to taking minutes, sharing files, etc."
+
+            },
+            {
+                "name": "Semantics2",
+                "app": "Air Wolf",
+                "stack": "Arduino Mega, GPS, Server: Perl, Selenium, Client Side: Yql, jQuery APIs: FB graph, GMaps, OpenCV, face.com Justin.tv",
+                "description": "A cost-effective UAV with a smartphone on board. Does real time image processing, facial recognition."
+            },
+            {
+                "name": "Semantics3",
+                "app": "Vigilance",
+                "stack": "Frontend: iOS, Android, Hoiio API for SMS, UIKit. Backend: node.js, Perl calling script, searches, etc. Limestone",
+                "description": "A mobile app on Android which has multiple use cases. When kids roam around, data about sex offenders, arson etc can be used to plot dangerous areas, and indicate danger level of area according to algorithm, sending an SMS to parents if critical."
+            },
+            {
+                "name": "Team OS",
+                "app": "Study Buddy",
+                "stack": "HTML5, CSS3, App engine",
+                "description": "App to help students find other people in their modules they may not already know. Student meeting portal, give venue, assignment, etc. Planning to implement geotagging in future and give suggestions based on location. Learning algorithm, prefers people you enjoyed studying with."
+            },
+            {
+                "name": "tomato",
+                "app": " Taskcards",
+                "stack": " HTML5, Backbone.js",
+                "description": "A todo-list management application."
+            },
+            {
+                "name": "Berserks",
+                "app": "Berserk IVLE Forum",
+                "stack": "Django backend, jQuery, IVLE API",
+                "description": "IVLE Forum frontend — A new way to chat on IVLE."
+            },
+            {
+                "name": "113",
+                "app": "Module Graph",
+                "stack": "Flask, MongoDB, HTML5 Canvas API",
+                "description": "A module visualisation tool, see which modules you need to take to satisfy prerequisites for a module and what you can take from then on. A different way to get module information."
+            },
+            {
+                "name": "DLL",
+                "app": "Tutorials with Friends.",
+                "stack": "LAMP, Codeigniter",
+                "description": "Given a number of users, app will match timetable of all users and find tutorial slots that everyone can take."
+            },
+            {
+                "name": "GiveUsPlaybooks!",
+                "app": "Cab 2gether",
+                "stack": "LAMP, Hoiio, Android, HTML5",
+                "description": "Application for people who want to share cabs. Future growth into car pooling. You have company, you save cash, and you save the environment."
+            },
+            {
+                "name": "N-Team",
+                "app": "Geek Rave",
+                "stack": "LAMP, Codeigniter",
+                "description": "A social platform for NUS students to showcase projects they’ve done."
+            },
+            {
+                "name": "Jabtunes",
+                "app": "Orbiter",
+                "stack": "HTML5, Node.js, Couch DB, Express, Orbit",
+                "description": "Cloud-based collaborative platform for game development."
+            },
+            {
+                "name": "Team Kickbutt!",
+                "app": "I CORSed it",
+                "stack": "Ruby on Rails",
+                "description": "Provides an analytical view of past bidding of modules’ stats."
+            },
+            {
+                "name": "Veeraya",
+                "app": "Veeraya",
+                "stack": "Ruby on Rails",
+                "description": "Online food-ordering system."
+            }
+
+        ];
+};
+exports.teams = teams();
