54bf719c2d0315de7a78a7f3dfed090ea3ead749
Harish V <harishv207@gmail.com>
2017-10-07 14:06:17 +0800

    Added sponsor-kit for H&R 2018

---
+++ 2018/assets/js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$(".tlt").textillate({
+	in: {
+	// set the effect name
+	effect: 'fadeInLeftBig',
+
+	// set the delay factor applied to each consecutive character
+	delayScale: 1.5,
+
+	// set the delay between each character
+	delay: 50,
+
+	// set to true to animate all the characters at the same time
+	sync: false,
+
+	// randomize the character sequence
+	// (note that shuffle doesn't make sense with sync = true)
+	shuffle: true,
+
+	// reverse the character sequence
+	// (note that reverse doesn't make sense with sync = true)
+	reverse: false,
+
+	// callback that executes once the animation has finished
+	callback: function () {}
+}
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+});
